3ef7eb5cc22bf4df75a0abbc634393410169e16e
Zia Rehman <iamziarehman@gmail.com>
2023-08-12 00:45:01 +0100

    Added documentation to TradingEngineServerConfiguration

--- src/TradingEngineServer/TradingEngineServerConfiguration/TradingEngineServerConfiguration.cs
+++ src/TradingEngineServer/TradingEngineServerConfiguration/TradingEngineServerConfiguration.cs
@@ -5,12 +5,18 @@ using System.Threading.Tasks;
 
 namespace TradingEngineServer.Core.Configuration
 {
+    /// <summary>
+    /// Representation of the Trading Engine Server Configuration
+    /// </summary>
     public class TradingEngineServerConfiguration
     {
         public TradingEngineServerSettings TradingEngineServerSettings { get; set; }
         
     }
 
+    /// <summary>
+    /// Trading Engine Server Settings
+    /// </summary>
     public class TradingEngineServerSettings
     {
         public int Port { get; set; }
