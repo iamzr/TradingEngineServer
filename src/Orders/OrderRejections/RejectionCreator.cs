38a3cb24ddd8e0199262afbd7f7fa62fe482139e
Zia Rehman <iamziarehman@gmail.com>
2023-08-12 14:08:26 +0100

    Fixed import in RejectionCreator

--- src/Orders/OrderRejections/RejectionCreator.cs
+++ src/Orders/OrderRejections/RejectionCreator.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TradingEngineServer.Orders;
 
 namespace TradingEngineServer.Rejects
 {
     public class RejectionCreator
     {
-        public static Rejection GenerateOrderCoreRejection(IOrderCore orderCore, RejectionReason rejectionReason)
+        public static Rejection GenerateOrderCoreRejection(IOrderCore rejectedOrder, RejectionReason rejectionReason)
         {
             return new Rejection(rejectedOrder, rejectionReason);
         }
