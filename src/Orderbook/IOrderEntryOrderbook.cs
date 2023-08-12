43ba74a04f73de62cf93fbb8d4a5a3c708c510cb
Zia Rehman <iamziarehman@gmail.com>
2023-08-12 14:25:48 +0100

    Added IOrderEntryOrderbook

---
+++ src/Orderbook/IOrderEntryOrderbook.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TradingEngineServer.Orders;
+
+namespace TradingEngineServer.Orderbook
+{
+    public interface IOrderEntryOrderbook : IReadOnlyOrderbook
+    {
+        void AddOrder(Order order);
+        void ChangeOrder (ModifyOrder modifyOrder);
+        void RemoveOrder(CancelOrder cancelOrder);
+    }
+}
